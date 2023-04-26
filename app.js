200f2ba9bbc34a39081f7c0927a59012f1d46d2f
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-04-26 04:36:34 +0100

    /subscriptions route webhook

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const app = express();
 const { connectDB } = require("./config/database.config")
+const User = require("./models/user.model");
+const WooCommerceAPI = require('woocommerce-api');
 require("dotenv").config()
 
 const port = process.env.PORT || 5050;
+
+// Set up WooCommerce API connection
+const WooCommerce = new WooCommerceAPI({
+    url: 'https://yourstore.com',
+    consumerKey: 'your_consumer_key',
+    consumerSecret: 'your_consumer_secret',
+    wpAPI: true,
+    version: 'wc/v3',
+});
+
+// Define webhook endpoint to receive subscription updates
+app.post('/subscriptions', async (req, res) => {
+    try {
+        const { phone, subscriptionId } = req.body;
+
+        // Query user model for user with given phone number
+        const user = await User.findOne({ where: { phone } });
+        if (!user) {
+            return res.status(400).send('User not found');
+        }
+        const Subscription = await user.getSubscription();
+
+        // Update subscription model for user
+        const subscription = await WooCommerce.get(`subscriptions/${subscriptionId}`);
+        await Subscription.update({ status: subscription.status }, { where: { userId: user.id } });
+
+        return res.status(200).send('Subscription updated');
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Internal server error');
+    }
+});
 
 (async () => {
     try {
