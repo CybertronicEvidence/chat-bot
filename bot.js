2857843136bf13e64ece87a9c69cedc07d9755a5
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-04-26 03:33:53 +0100

    whisper transcription feature

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const openai = require("./config/openai.config")
+const fs = require("fs");
 const { connectDB } = require("./config/database.config")
 const User = require("./models/user.model");
 const Subscription = require("./models/subscription.model")
 
-// // Sync database schema
-// sequelize.sync({ force: true }).then(() => {
-//     console.log('Database schema synced');
-// });
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 // Initialize WhatsApp client
 const client = new Client({
@@ -35,6 +35,8 @@ client.on('message', async (msg) => {
     const chat = await msg.getChat();
     chat.sendStateTyping();
 
+    // check for audio file
+
     // Check if user is registered
     const user = await checkUserRegistration(msg.from);
     if (!user) {
@@ -52,6 +54,74 @@ client.on('message', async (msg) => {
             return;
         }
 
+        if (msg.hasMedia) {
+            console.log(msg);
+
+            console.log("Media");
+            // download media into /voice folder
+            const media = await msg.downloadMedia();
+            console.log(media);
+
+            const filePath = `./voice/voice${randomInt()}.ogg`;
+            const buffer = Buffer.from(media.data, 'base64');
+
+            fs.writeFileSync(filePath, buffer)
+            console.log(`wrote ${buffer.byteLength.toLocaleString()} bytes to file.`)
+
+            // convert to mp3
+            await ffmpeg(filePath)
+                .audioCodec('libmp3lame')
+                .audioBitrate('128k')
+                .on('error', err => console.error('Error: ', err))
+                .on('end', async () => {
+
+                    console.log('Conversion to mp3 completed')
+                    // transcribe voice to text
+                    let resp = {}
+                    try {
+                        resp = await openai.createTranscription(
+                            fs.createReadStream(filePath.replace('.ogg', '.mp3')),
+                            "whisper-1",
+                            "en-US"
+                        );
+
+                        // transcribed text
+                        const messages = await resp.data.text;
+
+                        // generate response for transcribed text
+                        const message = await generateResponse(messages);
+
+                        // send message to user
+                        await chat.sendMessage(message);
+                        console.log('Transcription: ' + msg_to_process);
+                    } catch (e) {
+                        console.log(e);
+                    }
+
+                    try {
+                        // Delete voice file
+                        fs.unlink(filePath, (err) => {
+                            if (err) {
+                                console.log('Error deleting ' + filePath);
+                                return
+                            }
+                            console.log(`${filePath} was deleted`)
+                        })
+
+                        fs.unlink(filePath.replace('.ogg', '.mp3'), (err) => {
+                            if (err) {
+                                console.log('Error deleting ' + filePath.replace('.ogg', '.mp3'));
+                                return
+                            }
+                            console.log(`${filePath.replace('.ogg', '.mp3')} was deleted`)
+                        })
+
+                    } catch (error) {
+                        console.log(error)
+                    }
+                })
+                .save(filePath.replace('.ogg', '.mp3'));
+        }
         // Generate response using OpenAI
         const response = await generateResponse(msg.body);
 
