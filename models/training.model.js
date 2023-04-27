0b59982d8014e0f65693b3d1014cb2a39eeb0419
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-04-27 15:06:53 +0100

    updated model

--- models/training.model.js
+++ models/training.model.js
@@ -4,7 +4,7 @@ const { sequelize } = require("../config/database.config");
 const Training = sequelize.define(
     'training',
     {
-        trainingData: {
+        data: {
             type: DataTypes.STRING
         }
     }
