d45ca8f35690356338849cd5d93317d90c6ee903
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-05-06 19:54:02 +0100

    removed DROP IF from database

--- config/database.config.js
+++ config/database.config.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(db, db_user, db_pass, {
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
-        await sequelize.sync({ force: true });
+        await sequelize.sync();
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
